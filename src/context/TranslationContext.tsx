a00c2b7c0256eb8bde033f16f6c40eb1f0d7c4e4
Lucio Berté <lucioaberte@gmail.com>
2024-02-19 17:26:34 -0300

    translating storage

--- src/context/TranslationContext.tsx
+++ src/context/TranslationContext.tsx
@@ -28,19 +28,28 @@ export const useTranslation = () => {
 };
 
 export const TranslationProvider: React.FC<Props> = ({ children }) => {
-  const [language, setLanguage] = useState<'en' | 'es'>('en');
+  const storedLanguage = localStorage.getItem('language');
+  const [language, setLanguage] = useState<'en' | 'es'>(
+    (storedLanguage && (storedLanguage === 'en' || storedLanguage === 'es')) ? storedLanguage : 'en'
+  );
   const [loadingTranslation, setLoadingTranslation] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('language', language);
+  }, [language]);
 
   const t = (key: TranslationKeys) => {
     return translations[language][key] || key;
   };
 
   return (
-    <TranslationContext.Provider value={{ language, setLanguage, t, loadingTranslation, setLoadingTranslation }}>
+    <TranslationContext.Provider
+      value={{ language, setLanguage, t, loadingTranslation, setLoadingTranslation }}
+    >
       {loadingTranslation && (
         <div className="loading-overlay">
           <div className="spinner"></div>
-            <p>{t("Loading")}</p>
+          <p>{t("Loading")}</p>
         </div>
       )}
       {children}
